dad02d11f935a46216d074f13ebe9f40f5c06b41
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-12-30 15:25:07 +0100

    Dolne menu aplikacji

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { ITEMS_DATA } from "./data/items";
 import ItemListPosition from "./components/ItemListPosition";
+import BottomMenu from "./components/BottomMenu/BottomMenu";
 
 function App() {
     return (
         <div>
             {ITEMS_DATA.map( (item) => <ItemListPosition {...item} /> ) }
+            <BottomMenu />
         </div>
     );
 }
---
+++ src/components/BottomMenu/BottomMenu.jsx
@@ -0,0 +1,10 @@
+import './BottomMenu.css';
+function BottomMenu() {
+    return (
+        <div className="bottom-menu">
+            <p>Tutaj będzie menu</p>
+        </div>
+    );
+}
+
+export default BottomMenu;
