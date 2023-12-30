1ed97db5bc19ec2c4deb9cdad5845b826b7dba5a
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-12-30 15:33:41 +0100

    Strona główna - zawartość dobierana dynamicznie

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import BottomMenu from "./components/BottomMenu/BottomMenu";
 import ItemList from "./components/Items/ItemList";
 
+
 function App() {
+    const [ selectedContent, handleSelectedConted ] = useState('MainPage');
+
+    let mainContent = '';
+    if (selectedContent == 'MainPage') {
+        mainContent = <MainPage />
+    } else if (selectedContent == 'ItemList') {
+        mainContent = <ItemList />
+    }
+
     return (
         <div>
-            <ItemList />
+            {mainContent}
             <BottomMenu />
+        </div>
+    );
+}
+
+function MainPage() {
+    return (
+        <div>
+            Tutaj strona główna
         </div>
     );
 }
